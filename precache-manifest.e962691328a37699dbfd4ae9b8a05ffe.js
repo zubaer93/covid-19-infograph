de901afc0f333a776c796d2e4a1fa53e5443c7a6
zubaer <zubaer@stylinecollection.com>
2020-04-25 14:28:08 +0600

    Updates

---
+++ precache-manifest.e962691328a37699dbfd4ae9b8a05ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3baad4c6e03e797c95bdbef9484c93c5",
+    "url": "/covid-19-infograph/index.html"
+  },
+  {
+    "revision": "17241a73fb3a48cc9dd2",
+    "url": "/covid-19-infograph/static/css/main.0334e92a.chunk.css"
+  },
+  {
+    "revision": "6b7ca8838ec09f4cc9d9",
+    "url": "/covid-19-infograph/static/js/2.dae8b5f9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-infograph/static/js/2.dae8b5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17241a73fb3a48cc9dd2",
+    "url": "/covid-19-infograph/static/js/main.b12efeee.chunk.js"
+  },
+  {
+    "revision": "467b05e73c33fdf038b0",
+    "url": "/covid-19-infograph/static/js/runtime-main.6b9026ca.js"
+  },
+  {
+    "revision": "f307723460b5a0472e4194bbc7dec617",
+    "url": "/covid-19-infograph/static/media/image.f3077234.png"
+  }
+]);
